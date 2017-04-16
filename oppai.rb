9f5707e8ba7702ea35a0a58db17e239b4fe77307
Peter Rowlands <peter@pmrowla.com>
2017-04-17 07:14:23 +0900

    [oppai] point head to upstream

--- oppai.rb
+++ oppai.rb
@@ -3,7 +3,7 @@
 class Oppai < Formula
   desc "osu! pp advanced inspector (oppai) is a difficulty and pp calculator for osu! standard beatmaps."
   homepage "https://github.com/Francesco149/oppai"
-  head "https://github.com/pmrowla/oppai.git", :branch => "build-osx"
+  head "https://github.com/Francesco149/oppai.git"
   url "https://github.com/Francesco149/oppai/archive/0.9.3.tar.gz"
   sha256 "3753c3346202e08dddffb8e70714084775bd37ada7fd5d22e5a336a7290922d2"
 
