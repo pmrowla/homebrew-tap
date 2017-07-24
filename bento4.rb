497f107c4c09155e1b65c381528dd92e9f485585
Peter Rowlands <peter@pmrowla.com>
2017-07-24 18:22:33 +0900

    [bento4] - remove homebrew-core bottle references

--- bento4.rb
+++ bento4.rb
@@ -6,15 +6,7 @@ class Bento4 < Formula
   sha256 "109d48b75e7ba34d5a0cb98346c098d9e0d29d58b6bc27b90014dbc558a491a4"
   head "https://github.com/axiomatic-systems/Bento4.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "a8804fd92f0b384287b762dbb7b5ceb9f6f98c4859ad3d62abd0857e02f65448" => :sierra
-    sha256 "436e5973c42b9b788fa431e51b5b04d6acc4a91ff015a070a35e308e23db323e" => :el_capitan
-    sha256 "bdc17a166e54cface41c9e16227c1d18800fcd077f9c8d4fbb0cd111653856ec" => :yosemite
-  end
-
   depends_on :xcode => build
-
   conflicts_with "gpac", :because => "both install `mp42ts` binaries"
 
   def install
